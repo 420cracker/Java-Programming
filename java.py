457f4c619e44abde99c70a370173e6e573076980
Muhammad Haris <90536191+420cracker@users.noreply.github.com>
2022-05-18 23:23:30 -0700

    java.py

---
+++ java.py
@@ -0,0 +1,24 @@
+class FirstLab{
+  public static void main (String [] args){
+
+
+  System.out.println("Asslam O Alaikum");
+  System.out.println("Starting with the name of ALLAH!");
+
+
+
+System.out.println("IN the first lab we will learn about addition, multiplication, division, subtraction and reminder in java .So, we have some examples:");
+
+System.out.println(3+7*4);
+System.out.println(12/4*2);
+System.out.println(10/6.0*3);
+
+
+                 System.out.println("Furthur we will discuss about the difference between \"println\" and \"Print\":");
+
+                 System.out.println("My name is Haris");                 //write this statwment and note it's output.
+                 System.out.print("My name is Haris");                   //write this statement and note it's output.
+                                                       //note the difference between these two statements.
+
+
+                                                       System.out.println("End of First Lab \n JAZAKALLAH ");
