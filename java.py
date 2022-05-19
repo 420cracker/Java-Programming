dec680b167b81c65fde5f2245afbb1e21991a81f
Muhammad Haris <90536191+420cracker@users.noreply.github.com>
2022-05-18 23:47:58 -0700

    Update java.py

--- java.py
+++ java.py
@@ -22,3 +22,531 @@ System.out.println(10/6.0*3);
 
 
                                                        System.out.println("End of First Lab \n JAZAKALLAH ");
+    
+    
+    2nd LAB:
+    
+    
+      import java.util.Scanner;
+
+  class SecondLab{
+      public static void main (String [] args){
+
+    //  System.out.println("Asslam O Alaikum");
+     // System.out.println("Starting with the name of ALLAH");
+
+
+    //  System.out.println("Muhammad.\nHaris. \nHaroon. ");
+    //  System.out.println("1234\r56");
+    //  System.out.println("Haris\t78");
+    //  System.out.println("sum ==\b89");
+    //  System.out.println("my favourite game is \"Cricket\".");
+    //  System.out.println("let\'s walk!");
+    //  System.out.println("He has given RS.1000 \\-");
+
+
+
+      //TASK1 = Print "1" to "5" using single print statement.
+
+//      System.out.println("1:\n2:\n3:\n4\n5:");
+
+      //TASK2 = Print following paragraph using multiple print statements:
+
+//      System.out.print("Sajid is a bank Employee.");
+//      System.out.print("Sajid\'s designation is cashier.");
+//      System.out.print("He earns RS.40,000 \\-per month.");
+//      System.out.println("He lives in \"Gelberg\"");
+
+
+                        //TASK3= take two input from user and print their sum, difference, multiplication, division and reminder:
+
+                                 //SUM OPERATION:
+
+               //     Scanner sc = new Scanner (System.in);
+                //    System.out.println("please enter first number:");
+                //    int num = sc.nextInt ();
+               //    System.out.println("please enter second number:");
+               //    int num2 = sc.nextInt();
+               //    System.out.println(num + "+" + num2);
+                //  System.out.println(num + num2);
+
+
+                          //DIFFERENCE OPERATION:
+
+
+                      //Scanner sc = new Scanner (System.in);
+                // System.out.println("please enter first number:");
+				                   //  int num = sc.nextInt ();
+				                  //  System.out.println("please enter second number:");
+				                   // int num2 = sc.nextInt();
+				                  //  System.out.println(num + "-" + num2);
+                 //  System.out.println(num - num2);
+
+
+                   //MULTIPLICATION OPERATION:
+
+
+                //   Scanner sc = new Scanner (System.in);
+				                 //   System.out.println("please enter first number:");
+				   				 //                    int num = sc.nextInt ();
+				   				            //       System.out.println("please enter second number:");
+				   				            //        int num2 = sc.nextInt();
+				   				            //       System.out.println(num + "*" + num2);
+				                 //   System.out.println(num * num2);
+
+
+
+                                //DIVISION OPERATION:
+
+
+
+                                                                          // Scanner sc = new Scanner (System.in);
+                                                                            //  System.out.println("please enter first number:");
+												   				                 //      int num = sc.nextInt ();
+												   				                //    System.out.println("please enter second number:");
+												   				                //    int num2 = sc.nextInt();
+												   				                //    System.out.println(num + "/" + num2);
+				                                                   //System.out.println(num / num2);
+
+
+
+                                 //REMINDER OPERATION:
+
+
+
+
+
+                            // Scanner sc = new Scanner (System.in);
+				                   // System.out.println("please enter first number:");
+				   				               //    int num = sc.nextInt ();
+				   				               //   System.out.println("please enter second number:");
+				   				                //   int num2 = sc.nextInt();
+				   				                  //  System.out.println(num + "%" + num2);
+				                     // System.out.println(num % num2);
+
+
+                 //TASK4 :
+
+
+                 //DISPLAYING DATE:
+
+               //  Scanner sce = new Scanner (System.in);
+              //  System.out.println("please enter today date:");
+             //    int date = sce.nextInt();
+                // System.out.println("please enter today month");
+            //    int month = sce.nextInt();
+                //System.out.println(date + "/" + month + "/" + 2022);
+
+
+                     
+
+
+
+                       //      Scanner sce = new Scanner (System.in);
+				             //   System.out.println("please enter today date:");
+				              //   int date = sce.nextInt();
+				              //   System.out.println("please enter today month");
+				             //  int month = sce.nextInt();
+				              // System.out.println("please enter year");
+				             // int year = sce.nextInt();
+				             // System.out.println(date + "-" + month + "-" + year);
+
+
+                    //DISPLAYING TIME:
+
+
+
+                 //   Scanner scan = new Scanner (System.in);
+                 //   System.out.println("please enter time hour:");
+                //    int hour = scan.nextInt();
+               //  System.out.println("please enter time minute:");
+               //    int minute = scan.nextInt();
+               // System.out.println("please enter time second:");
+                //   int second = scan.nextInt();
+
+                  // System.out.println(hour + ":" + minute + ":" + second);
+
+
+
+                       // Scanner scan = new Scanner (System.in);
+                   //  System.out.println("please enter time hour:");
+				                //       int hour = scan.nextInt();
+				                 //  System.out.println("please enter time minute:");
+				                 //     int minute = scan.nextInt();
+				                //  System.out.println("please enter time second:");
+				                 //     int second = scan.nextInt();
+
+                  //  System.out.println(hour + "hour" + " " + minute + "minute" + " " + second + "seconds");
+
+
+
+
+                   System.out.println("END OF SECOND LAB: \n JAZAKALLAH");
+        
+        
+        3rd LAB:
+        
+        import java.util.Scanner;
+class ThirdLab{
+
+       public static void main (String args[]){
+
+
+
+       //LAB3
+
+       //TASK1:
+
+       System.out.print ( (int)Math.pow(2,1));
+       System.out.print(" "+(int)Math.pow(2,2));
+       System.out.print (" "+(int)Math.pow(2,3));
+       System.out.println (" "+ (int)Math.pow(2,4));
+
+
+
+       System.out.print ((int)Math.pow(3,1));
+       System.out.print  ( " " + (int)Math.pow(3,2));
+       System.out.print ( " " + (int)Math.pow(3,3));
+       System.out.println (" " + (int)Math.pow(3,4));
+
+
+
+
+
+
+
+       System.out.print((int)Math.pow(4,1));
+       System.out.print( " " +(int)Math.pow(4,2));
+       System.out.print( " " +(int)Math.pow(4,3));
+       System.out.println( " "+(int)Math.pow(4,3));
+
+       //TASK2:
+
+       Scanner sc = new Scanner(System.in);
+      // System.out.println("please enter \"a\" value:");
+      // double a = sc.nextDouble();
+      // System.out.println("please enter \"b\" value:");
+      // double b = sc.nextDouble();
+      // double c = 5;
+      // System.out.println("c� = a� + b�");
+      // System.out.println(c + "=" + a + "+" + b);
+      // System.out.println( (a*a) + (b*b) + "=" + c*c);
+
+
+
+           //TASK3:
+
+ Scanner scan = new Scanner(System.in);
+System.out.println("please enter your Basic Salary:");
+int x = scan.nextInt();
+System.out.println("Salary Details:");
+
+double houserent = (x*0.45);
+double medicalallowence = (x*0.10);
+double grosssalary = (houserent + medicalallowence + x);
+double providentfund = (x*0.05);
+double netsalary = (grosssalary-providentfund);
+System.out.print("House Rent:       ");
+System.out.println(houserent);
+System.out.print("Medical Alloance:    ");
+System.out.println(medicalallowence);
+System.out.println("               " + "----------------");
+System.out.print("Gross Salary:     ");
+System.out.println(grosssalary);
+System.out.println("\n  \n ");
+System.out.print("Provident Fund:    ");
+System.out.println(providentfund);
+System.out.println("                " + "------------");
+System.out.print("Net Salary:      ");
+System.out.println(netsalary);
+
+
+
+       //TASK4:
+
+              //     String space = "                  ";
+              //     System.out.println(space.substring(0,0) + "*");
+              //     System.out.println(space.substring(0,1) + "*");
+              //     System.out.println(space.substring(0,2) + "*");
+              //     System.out.println(space.substring(0,3) + "*");
+              //     System.out.println(space.substring(0,4) + "*");
+              //     System.out.println(space.substring(0,5) + "*");
+
+
+              //TASK5:
+
+
+                  //  String s = "This is 3rd lab";
+                  //  int pos1=0,pos2,pos3,pos4;
+                  //  pos2 = s.indexOf(' ');
+                  //  System.out.println(s.substring(pos1,pos2));
+                  //  pos1 =pos2+1;
+                  //  pos2 =s.indexOf(' ',pos1);
+                  //  System.out.println(s.substring(pos1,pos2));
+
+                  //  pos1 =pos3+1;
+				//	                    pos2 =s.indexOf(' ',pos2);
+                  //  System.out.println(s.substring(pos2,pos3));
+
+
+
+       }
+       }
+
+
+                         HOMEWORK1:
+        
+         class Homewor1{
+     public static void main (String [] args){
+
+
+
+
+
+
+     //HOMEWORK 1
+
+     //5 table
+
+    //int a = 5*1;
+    //int b = 5*2;
+    //int c =5*3;
+    //int d = 5*4;
+    //int e = 5*5;
+    //int f = 5*6;
+    //int g = 5*7;
+    //int h = 5*8;
+    //int i = 5*9;
+    //int j = 5*10;
+  //  System.out.println("5 * 1 = "+a + "\n " + "5 * 2 = "+b + "\n" + "5 * 3 = "+c +  "\n" +"5 * 4 = "+d  +"\n"+ "5 * 5 = " +e+ "\n" + "5 * 6 = " +f + "\n" + "5 * 7 = " +g + "\n" + "5 * 8 = "+h + "\n"+"5 * 9 = "+i + "\n" + "5 * 10 = "+j);
+
+
+     //task2:
+
+      //System.out.println("1 \n 2\n  3\n   4\n    5");
+
+
+
+
+              //TASK3:
+
+
+           //    System.out.println(" 1     1\n  2     2\n   3    3\n    4    4\n     5 5 ");
+
+
+
+
+
+                //TASK5:
+
+
+
+        //        System.out.println("5/2");
+           //     System.out.println("5+3/2.0");
+           //     System.out.println("5+3/1.5+2.5");
+            //    System.out.println("5/2.0 - 3/2.0");
+
+
+
+
+            //TASK5:
+
+
+      //      System.out.println("2 = 3 and 3 < 5");
+      //      System.out.println("5 != 3 or 3 == 6 ");
+      //      System.out.println("not 5 != 3 or == 6");
+      //      System.out.println("not5 !=3 or Not 3==6");
+
+      //      System.out.println(!(5!=3) || !(3==6));
+
+
+     }
+     }
+
+                   HOMEWORK2:
+        
+        
+        import java.util.Scanner;
+class HomeWork2{
+
+    public static void main(String args[]){
+
+               //TASK1:
+
+
+   //  double a = (Math.pow(2,1));
+   //  double b = (Math.pow(2,2));
+   //  double c = (Math.pow(2,3));
+   //double d = (Math.pow(2,4));
+   //double e = (Math.pow(2,5));
+//System.out.println("2^1 =" +a +"\n"+ "2^2 =" +b + "\n" + "2^3 ="+c + "\n" + "2^4 ="+d + "\n" + "2^5 ="+e);
+
+
+           //TASK2:
+
+
+//Scanner sc = new Scanner(System.in);
+//System.out.println("put the value of r =");
+//int r = sc.nextInt();
+
+//System.out.println("Area of square =" + r*r);
+//System.out.println("Area of circle ="+ (22/7)*r*r);
+//System.out.println("Area of sector ="+ 1/2*  (r*r)*Math.toRadians(90/360) );
+
+
+
+ //TASK3:
+
+
+// Scanner tr = new Scanner(System.in);
+// System.out.println("please write '0' value in degree: ");
+// int degree = tr.nextInt();
+// double cos = (Math.toRadians(degree));
+// System.out.println("Cos0 ="+Math.cos(cos));
+// double sin = (Math.toRadians(degree));
+// System.out.println("Sin0 ="+ Math.sin(sin));
+// double tan = (Math.toRadians(degree));
+// System.out.println("Tan0 ="+ Math.tan(tan));
+
+                            //TASK4:
+
+
+//String s = "Welcome";
+//System.out.println(s.substring(0,7));
+//System.out.println(s.substring(0,6));
+//System.out.println(s.substring(0,5));
+//System.out.println(s.substring(0,4));
+//System.out.println(s.substring(0,3));
+//System.out.println(s.substring(0,2));
+//System.out.println(s.substring(0,1));
+//System.out.println(s.substring(0,0));
+
+
+                                    //TASK5:
+
+
+
+             //   String a = "Welcome";
+             //   System.out.print(a.charAt(0));
+             //   System.out.print(" ");
+             //   System.out.print(a.charAt(1));
+             //   System.out.print(" ");
+             //   System.out.print(a.charAt(2));
+             //   System.out.print(" ");
+             //   System.out.print(a.charAt(3));
+             //   System.out.print(" ");
+             //   System.out.print(a.charAt(4));
+             //   System.out.print(" ");
+             //   System.out.print(a.charAt(5));
+             //   System.out.print(" ");
+             //   System.out.println(a.charAt(6));
+
+
+
+                                       //TASK6:
+
+       //   String t = "Welcome";
+       //   System.out.println(t);
+       //   System.out.println("LoweCase ="+ t.toLowerCase());
+        //   System.out.println("UpperCase ="+ t.toUpperCase());
+
+
+
+
+
+
+
+
+
+
+                                     //Task7:
+
+                            //         String s1 = "Welcome";
+                              //      s1 = s1.concat("MIT");
+                               //      System.out.println(s1);
+
+
+    }
+        }
+                              HOMEWORK3:
+        
+          import java.util.Scanner;
+   class HomeWork3{
+
+             public static void main(String args[]){
+
+                                                                 //1st task1
+           //  String space = "                     ";
+           // System.out.println("*");
+             //System.out.println("*" + space.substring(0,0) + "*");
+             //System.out.println("*" + space.substring(1,2) + "*");
+             //System.out.println("*" + space.substring(1,3) + "*");
+             //System.out.println("*" + space.substring(1,4) + "*");
+             //System.out.println("*" + space.substring(1,5) + "*");
+             //System.out.println("*" + space.substring(1,6) + "*");
+             //System.out.println("*" + space.substring(1,7) + "*");
+             //System.out.println("* * * * *");
+
+
+             //Task2:
+
+         //  String s = "         ";
+         //  System.out.println(s.substring(0,5) +"*");
+
+         //  System.out.println("*"+ s.substring(0,4)+ "*"+ s.substring(4,5)+ "*");
+
+
+                                  //Task3:
+                                  //print 5 rel value between 200 and 300,
+
+                          System.out.println(Math.random()*100+200);
+
+                          //Task4:
+                          //print 5 int vlues between -200 nd -100,
+                          System.out.println((int)Math.random()*(-100)+(-200));
+
+
+                          //TASK5:
+
+                 //         ASCII CODES
+
+                      System.out.println((char)67);
+                       System.out.println((char)78);
+                         System.out.println((char)77);
+                           System.out.println((char)79);
+                             System.out.println((char)82);
+
+
+
+                      //TASK 6:
+
+                      Scanner sc = new Scanner(System.in);
+                  //    System.out.println("plese write your first name: ");
+                  //    String s = sc.nextLine();
+                  //    System.out.println("please write your last name:  ");
+                  //    String s2 = sc.nextLine();
+                  //    System.out.println("your name is here:");
+                  //    System.out.println(s + "\n"+ s2);
+
+                           //task7:
+
+
+
+                    System.out.println("please write \"a\" value:  ");
+                    double a = sc.nextDouble();
+                    System.out.println("please write \"b\" value:  ");
+                    double b = sc.nextDouble();
+                    System.out.println("please write \"c\" value:  ");
+                    double c = sc.nextDouble();
+
+
+                     System.out.println(-b + (Math.sqrt(b*b-4*a*c)/a*a));
+
+
+
+                      System.out.println(-b - (Math.sqrt(b*b-4*a*c)/a*a));
+
+
+
+             }
+   }
